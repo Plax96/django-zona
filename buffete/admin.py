4cd5b4508bfbb360138ad0bc3044238d871dae31
Plax <placido.castellanos@gmail.com>
2017-11-03 23:20:36 -0600

    Primera parte

---
+++ buffete/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from buffete.models import *
+
+admin.site.register(Cliente)
+admin.site.register(Expediente)
+admin.site.register(Abogado)
+# Register your models here.
---
+++ buffete/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from django.utils import timezone
+
+class Abogado(models.Model):
+    usuario= models.ForeignKey('auth.User')
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    estado= models.BooleanField(default="True")
+    dpi = models.CharField(max_length=20)
+    telefono = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.nombre
+
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    estado= models.BooleanField(default="True")
+    dpi = models.CharField(max_length=20)
+    telefono = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.nombre
+    
+class Expediente(models.Model):
+    fecha_ingreso = models.DateTimeField(default=timezone.now)
+    fecha_inicio = models.DateTimeField(blank = True, null = True)
+    fecha_finalizacion = models.DateTimeField(blank=True, null=True)
+    estado = models.CharField(max_length=2,default="1")
+    abogado = models.ForeignKey(Abogado, on_delete = models.CASCADE)
+    cliente = models.ForeignKey(Cliente, on_delete = models.CASCADE)
+
+
+
+
+# Create your models here.
