cf6f4acd46741d9a9cb2a4ee7f452f959386d2d4
Plax <placido.castellanos@gmail.com>
2017-11-05 12:44:07 -0600

    subido 1

--- buffete/views.py
+++ buffete/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from .models import *
 
 def listarcasos(request):
-    return render(request, 'buffete/listarcasos.html',{})
+    casos=Expediente.objects.filter(fecha_ingreso__lte=timezone.now()).order_by('fecha_ingreso')
+    return render(request, 'buffete/listarcasos.html',{'casos':casos})
 # Create your views here.
