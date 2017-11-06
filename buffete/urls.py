e231f8ea7ed8e9808adf8c1c54d277dc447d8aad
Plax <placido.castellanos@gmail.com>
2017-11-06 15:25:57 -0600

    logueo listo

--- buffete/urls.py
+++ buffete/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     url(r'^caso/(?P<id>\d+)/$', views.detallecaso, name="detalle_caso"),
     url(r'^caso/nuevo/$', views.nuevocaso, name="nuevo_caso"),
     url(r'^cliente/nuevo/$', views.nuevocliente, name="nuevo_cliente"),
+    url(r'^usuario/nuevo/$', views.nuevousuario, name="usuario_nuevo"),
+    url(r'^abogado/nuevo/$', views.abogadonuevo, name="abogado_nuevo"),
     ]
--- buffete/views.py
+++ buffete/views.py
@@ -2,24 +2,55 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.models import User
 from .models import *
 from .forms import *
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def listarcasos(request):
     casos=Expediente.objects.filter(fecha_ingreso__lte=timezone.now(),estado='1').order_by('fecha_ingreso')
     return render(request, 'buffete/listarcasos.html',{'casos':casos})
 
+@login_required
 def detallecaso(request,id):
     caso = get_object_or_404(Expediente, id=id)
     return render(request, 'buffete/detalle_caso.html',{'caso':caso})
 
+@login_required
 def nuevocaso(request):
     expediente = ExpedienteForm()
     return render(request, 'buffete/caso_editar.html', {'form':expediente})
 
+@login_required
 def eliminar_cliente(request,id):
     cliente= get_object_or_404(Cliente, id=id)
     cliente.update(estado=False)
     return render(request, 'buffete/detalle_caso.html',{'caso':caso})
- 
+
+def nuevousuario(request):
+   if request.method == "POST":
+       usuario = UserForm(request.POST)
+       if form.is_valid():
+           usuario = usuario.save(commit=False)
+           usuario.author = request.user
+           usuario.save()
+           return redirect('abogado_nuevo')
+   else:
+       usuario = UserForm()
+       return render(request, 'buffete/abogado_nuevo.html', {'form':usuario})
+
+def abogadonuevo(request):
+  if request.method == "POST":
+      abogado = AbogadoForm(request.POST)
+      if form.is_valid():
+          abogado = abogado.save(commit=False)
+          abogado.author = request.user
+          abogado.save()
+          return redirect('listarcasos')
+  else:
+      usuario = AbogadoForm()
+      return render(request, 'buffete/listarcasos.html', {'form':usuario})
+
+
+@login_required
 def nuevocliente(request):
     if request.method == "POST":
         cliente = ClienteForm(request.POST)
